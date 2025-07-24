609787af5b32d7c003b1d9631875c2720a285cbe
jefra <jeframoscas@gmail.com>
2025-07-24 16:28:21 -0500

    cambioJefry

--- app/src/main/java/com/example/appfinanzas2/MainActivity.kt
+++ app/src/main/java/com/example/appfinanzas2/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! asdaasdasdsadass",
+        text = "Hello $name! asdJEFRYass",
         modifier = modifier
     )
 }
