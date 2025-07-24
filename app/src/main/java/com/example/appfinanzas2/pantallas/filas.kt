76adfd8939673a78ef715454c5b910eb02df94e2
LENOVO <anthonycon957@gmail.com>
2025-07-24 17:04:55 -0500

    avance de Filas

--- app/src/main/java/com/example/appfinanzas2/pantallas/filas.kt
+++ app/src/main/java/com/example/appfinanzas2/pantallas/filas.kt
@@ -1,3 +1,9 @@
 package com.example.appfinanzas2.pantallas
 
-//xd
+import androidx.compose.runtime.Composable
+import com.example.appfinanzas2.models.Transaccion
+
+@Composable
+fun Filas(transacciones: List<Transaccion>) {
+    // Componente reusable para mostrar items de transacción
+}
